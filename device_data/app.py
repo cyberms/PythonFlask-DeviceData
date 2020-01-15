ac311b03dda654fc9456af271bbeff2de89ac34c
Matias Sequeira <matiassequeira@gmail.com>
2020-01-15 11:47:03 -0800

    c

--- device_data/app.py
+++ device_data/app.py
@@ -1,14 +1,19 @@
-import eventlet
+import eventlet, json
 from flask import Flask, render_template
 from flask_mqtt import Mqtt
 from flask_socketio import SocketIO
+from random import seed
+from random import randint
+import base64, datetime
 
-PATH= 'db/jobs.sqlite'
+# seed random number generator
+seed(1)
 
 eventlet.monkey_patch()
 
 app = Flask(__name__, template_folder='./templates')
-app.config['MQTT_BROKER_URL'] = '47.97.228.120'
+app.config['MQTT_BROKER_URL'] = 'localhost'
+#app.config['MQTT_BROKER_URL'] = '47.97.228.120'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_REFRESH_TIME'] = 1.0
 
@@ -21,14 +26,26 @@ def handle_connect(client, userdata, flags, rc):
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
+
+    json_event= json.loads(message.payload.decode())
+
+    if json_event.get('data') == 'ZGV2aWNlTm90SGE=':
+        pressure_reading = randint(60, 80)
+    else:
+        pressure_reading = base64.b64decode(json_event.get('data'))
+        #pressure_reading = randint(60, 80)
+
     data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
+            topic=message.topic,
+            pressure_reading=pressure_reading,
+            datetime= datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            device= json_event.get('devEUI')
+            )
+
     # emit a mqtt_message event to the socket containing the message data
     socketio.emit('mqtt_message', data=data)
 
-@app.route('/')
+@app.route('/device_pressure')
 def index():
     return render_template('graph.html')
 
